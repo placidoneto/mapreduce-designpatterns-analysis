0a5233f9fa3338565f1f3c8ff885c69a5807834f
estacao_de_trabalho <estacao_de_trabalho@servidor-nudes>
2016-12-16 11:28:44 -0300

    Pushing Java Scripts to project's repository.

---
+++ mapreduce-designpatterns-analysis/src/mapreduce/designpatterns/analysis/MapreduceDesignpatternsAnalysis.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mapreduce.designpatterns.analysis;
+
+/**
+ *
+ * @author estacao_de_trabalho
+ */
+public class MapreduceDesignpatternsAnalysis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
